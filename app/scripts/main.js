7614228647d83fedc20493be6047a7551a44093d
Vasileios Chouliaras <v.chouliaras@netstudio.gr>
2016-08-05 11:44:43 +0300

    Ass js functionallity

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+(function($) {
+  $(function() {
+    $("#typed").typed({
+      stringsElement: $('body').find('.strings-to-type'),
+      startDelay: 500,
+      cursorChar: " _",
+      callback: function(){
+        $('body').find('.social').removeClass('hidden')
+          .css('opacity', 0).animate({'opacity': '1'}, 300);
+      }
+    });
+  });
+}) (jQuery);
