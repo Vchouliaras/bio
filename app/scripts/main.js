a619f05111422b3bed91a922fcb0fb265bbe4529
Vasileios Chouliaras <vasilis.chouliaras@gmail.com>
2017-01-15 23:42:31 +0200

    Removes DomContentedLoad Event

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,12 @@
 (function($) {
-  $(function() {
-    $('#typed').typed({
-      stringsElement: $('body').find('.strings-to-type'),
-      startDelay: 800,
-      typeSpeed: 50,
-      cursorChar: ' _',
-      callback: function(){
-        $('body').find('.social').removeClass('hidden')
-          .css('opacity', 0).animate({'opacity': '1'}, 400);
-      }
-    });
+  $('#typed').typed({
+    stringsElement: $('body').find('.strings-to-type'),
+    startDelay: 800,
+    typeSpeed: 50,
+    cursorChar: ' _',
+    callback: function(){
+      $('body').find('.social').removeClass('hidden')
+        .css('opacity', 0).animate({'opacity': '1'}, 400);
+    }
   });
 })(jQuery);
